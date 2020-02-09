db8ba46aad4093f5f2443fae5871a33bcb20a554
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2020-02-09 21:03:51 +0900

    Reafactor

--- src/Vector2.spec.ts
+++ src/Vector2.spec.ts
@@ -2,7 +2,7 @@ import Vector2 from '~/Vector2';
 
 describe('Test of Vector2', () => {
 
-  describe('default constructor', () => {
+  describe('Test of default constructor', () => {
     const v = new Vector2();
     it (`v.x == 0`, () => { expect(v.x).toBe(0); })
     it (`v.y == 0`, () => { expect(v.x).toBe(0); })
