43597cc9aaa7aa749e1b22c1c89f76b394e4ad44
kazumichi kuroda <kazumichi-kuroda@nekonecode.com>
2020-02-22 03:41:27 +0900

    Added Triangle

---
+++ src/Triangle.spec.ts
@@ -0,0 +1,479 @@
+import Triangle, { Type } from './Triangle';
+import Vector2 from './Vector2';
+
+const r2 = Math.sqrt(2);
+const r3 = Math.sqrt(3);
+const r4 = Math.sqrt(4);
+
+// データの定義
+const D = {
+  // ３点が原点(無効な三角形)
+  zero  : {
+    name:"zero",
+    points:[0, 0, 0, 0, 0, 0],
+    AB:{x:0, y:0}, BC:{x:0, y:0}, CA:{x:0, y:0},
+    a:0, b:0, c:0,
+    maxSideName:"", minSideName:"",
+    maxLength:0, minLength:0,
+    maxCornerName:"", minCornerName:"",
+    cosA: 0, cosB:0, cosC:0,
+    sinA: 0, sinB:0, sinC:0,
+    tanA: 0, tanB:0, tanC:0,
+    maxCornerCos:0, minCornerCos: 0,
+    maxCornerSin:0, minCornerSin: 0,
+    maxCornerTan:0, minCornerTan: 0,
+    radA: 0, radB:0, radC:0,
+    area: 0,
+    outerRadius:0, innerRadius:0,
+    center: Vector2.zero,
+    outerCenter: Vector2.zero,
+    innerCenter:Vector2.zero,
+    isInvalid:true,
+    type: Type.None,
+    toString: "A(0, 0), B(0, 0), C(0, 0)",
+  },
+  // ３点が(1, 1)、(無効な三角形)
+  one   : {
+    name:"one",
+    points:[1, 1, 1, 1, 1, 1],
+    AB:{x:0, y:0}, BC:{x:0, y:0}, CA:{x:0, y:0},
+    a:0, b:0, c:0,
+    maxSideName:"", minSideName:"",
+    maxLength:0, minLength:0,
+    maxCornerName:"", minCornerName:"",
+    cosA: 0, cosB:0, cosC:0,
+    sinA: 0, sinB:0, sinC:0,
+    tanA: 0, tanB:0, tanC:0,
+    maxCornerCos:0, minCornerCos: 0,
+    maxCornerSin:0, minCornerSin: 0,
+    maxCornerTan:0, minCornerTan: 0,
+    radA: 0, radB:0, radC:0,
+    area: 0,
+    outerRadius:0, innerRadius:0,
+    center: Vector2.zero,
+    outerCenter: Vector2.zero,
+    innerCenter:Vector2.zero,
+    isInvalid:true,
+    type: Type.None,
+    toString: "A(1, 1), B(1, 1), C(1, 1)",
+  },
+  // 底辺が3、高さが4の三角形
+  s34   : {
+    name:"s34",
+    points:[3, 4, 0, 0, 3, 0],
+    AB:{x:-3, y:-4}, BC:{x:3, y:0}, CA:{x:0, y:4},
+    a:3, b:4, c:5,
+    maxSideName:"AB", minSideName:"BC",
+    maxLength:5, minLength:3,
+    maxCornerName:"C", minCornerName:"A",
+    cosA: 4/5, cosB:3/5, cosC:0,
+    sinA: 3/5, sinB:4/5, sinC:1,
+    tanA: 3/4, tanB:4/3, tanC:Infinity,
+    maxCornerCos: 0, minCornerCos: 4/5,
+    maxCornerSin: 1, minCornerSin: 3/5,
+    maxCornerTan:Infinity, minCornerTan: 3/4,
+    radA: 0.6435011087932843, radB:0.9272952180016123, radC:1.5707963267948966,
+    outerRadius:2.5, innerRadius:1,
+    area: 6,
+    center     : new Vector2(2, 1.3333333333333333),
+    outerCenter: new Vector2(1.5, 1.9999999999999996),
+    innerCenter: new Vector2(2, 1),
+    isInvalid:false,
+    type: Type.Right,
+    toString: "A(3, 4), B(0, 0), C(3, 0)",
+  },
+  // 底辺が1、高さが1の三角形
+  s11   : {
+    name:"s11",
+    points:[1, 1, 0, 0, 1, 0],
+    AB:{x:-1, y:-1}, BC:{x:1, y:0}, CA:{x:0, y:1},
+    a:1, b:1, c:r2,
+    maxSideName:"AB", minSideName:"BC",
+    maxLength:r2, minLength:1,
+    maxCornerName:"C", minCornerName:"A",
+    cosA: 1/r2, cosB:1/r2, cosC:0, 
+    sinA: 1/r2, sinB:1/r2, sinC:1, 
+    tanA: 1, tanB:1, tanC:-Infinity, 
+    maxCornerCos: 0, minCornerCos: 1/r2,
+    maxCornerSin: 1, minCornerSin: 1/r2,
+    maxCornerTan: -Infinity, minCornerTan: 1,
+    radA: 0.785398, radB:0.785398, radC:1.5707963267948966,
+    area: 0.5,
+    outerRadius:0.7071067811865477, innerRadius:0.2928932188134524,
+    center     : new Vector2(0.6666666666666666, 0.3333333333333333),
+    outerCenter: new Vector2(0.49999999999999994, 0.5000000000000001),
+    innerCenter: new Vector2(0.7071067811865475, 0.2928932188134525),
+    isInvalid:false,
+    type: Type.Right,
+    toString: "A(1, 1), B(0, 0), C(1, 0)",
+  },
+  // 正三角形
+  equi  : {
+    name:"equi",
+    points:[0, r3, -1, 0, 1, 0], 
+    AB:{x:-1, y:-r3}, BC:{x:2, y:0}, CA:{x:-1, y:r3},
+    a:2, b:r4, c:r4,
+    maxSideName:"BC", minSideName:"AB",
+    maxLength:2, minLength:r4,
+    maxCornerName:"A", minCornerName:"C",
+    cosA: 1/2, cosB:1/2, cosC:1/2, 
+    sinA: r3/2, sinB:r3/2, sinC:r3/2, 
+    tanA: r3, tanB:r3, tanC:r3, 
+    maxCornerCos: 1/2 , minCornerCos: 1/2,
+    maxCornerSin: r3/2, minCornerSin: r3/2,
+    maxCornerTan: r3  , minCornerTan: r3,
+    radA: 1.0472, radB:1.0472, radC:1.0472,
+    area: r3,
+    outerRadius:1.1547005383792515, innerRadius:0.5773502691896256,
+    center     : new Vector2(0, 0.5773502691896257),
+    outerCenter: new Vector2(0, 0.5773502691896257),
+    innerCenter: new Vector2(0, 0.5773502691896257),
+    isInvalid:false,
+    type: Type.Acute,
+    toString: `A(0, ${r3}), B(-1, 0), C(1, 0)`,
+  },
+  // 適当な鋭角三角形
+  acute : {
+    name:"acute",
+    points: [0.5, 3, 0, 1, 2, 1],
+    AB:{x:-0.5, y:-2}, BC:{x:2, y:0}, CA:{x:-1.5, y:2},
+    a:2, b:2.5, c:Math.sqrt(4.25),
+    maxSideName:"CA", minSideName:"BC",
+    maxLength:2.5, minLength:2,
+    maxCornerName:"B", minCornerName:"A",
+    cosA: 0.6305926250944657, cosB:0.24253562503633297, cosC:0.6,
+    sinA: 0.7761140001162655, sinB:0.9701425001453319, sinC:0.8,
+    tanA: 1.2307692307692308, tanB:4, tanC:1.3333333333333335,
+    maxCornerCos:0.24253562503633297, minCornerCos: 0.6305926250944657,
+    maxCornerSin:0.9701425001453319 , minCornerSin: 0.7761140001162655,
+    maxCornerTan:4                  , minCornerTan: 1.2307692307692308,
+    radA: 0.8884797719201486, radB:1.3258176636680326, radC:0.9272952180016123,
+    area: 2,
+    outerRadius:1.288470508005519, innerRadius:0.6096117967977924,
+    center     : new Vector2(0.8333333333333333, 1.6666666666666665),
+    outerCenter: new Vector2(1, 1.8125000000000002),
+    innerCenter: new Vector2(0.7807764064044151, 1.6096117967977925),
+    isInvalid:false,
+    type: Type.Acute,
+    toString: `A(0.5, 3), B(0, 1), C(2, 1)`,
+  },
+  // 適当な鈍角三角形
+  obtuse: {
+    name:"obtuse",
+    points: [0, 3, -2, 3, 2, 1],
+    AB:{x:-2, y:0}, BC:{x:4, y:-2}, CA:{x:-2, y:2},
+    a:Math.sqrt(20), b:Math.sqrt(8), c:2,
+    maxSideName:"BC", minSideName:"AB",
+    maxLength:Math.sqrt(20), minLength:2,
+    maxCornerName:"A", minCornerName:"C",
+    cosA: -0.7071067811865477, cosB:0.8944271909999159, cosC:0.948683298050514,
+    sinA: 0.7071067811865477, sinB:0.44721359549995804, sinC:0.3162277660168374,
+    tanA: -1, tanB:0.5000000000000001, tanC:0.3333333333333327,
+    maxCornerCos:-0.7071067811865477, minCornerCos: 0.948683298050514,
+    maxCornerSin:0.7071067811865477 , minCornerSin: 0.3162277660168374,
+    maxCornerTan:-1                 , minCornerTan: 0.3333333333333327,
+    radA: 2.3561944901923453, radB:0.46364760900080615, radC:0.3217505543966416,
+    area: 2,
+    outerRadius:3.1622776601683804, innerRadius:0.43008148707802096,
+    center     : new Vector2(0, 2.333333333333333),
+    outerCenter: new Vector2(-1.0000000000000078, -1.665334536937739e-15),
+    innerCenter: new Vector2(-0.1781455848733054, 2.569918512921979),
+    isInvalid:false,
+    type: Type.Obtuse,
+    toString: `A(0, 3), B(-2, 3), C(2, 1)`,
+  }
+}
+
+describe('Test of Triangle', () => {
+
+  //---------------------------------------------------------------------------
+  // constructor
+  //---------------------------------------------------------------------------
+  describe('Test of constructors', () => {
+    describe('default constructor', () => {
+      const t = new Triangle();
+      
+      it(`t.A.x = 0`, () => { expect(t.A.x).toBe(0); })
+      it(`t.A.y = 0`, () => { expect(t.A.y).toBe(0); })
+      it(`t.B.x = 0`, () => { expect(t.B.x).toBe(0); })
+      it(`t.B.y = 0`, () => { expect(t.B.y).toBe(0); })
+      it(`t.C.x = 0`, () => { expect(t.C.x).toBe(0); })
+      it(`t.C.y = 0`, () => { expect(t.C.y).toBe(0); })
+    })
+  })
+
+  //---------------------------------------------------------------------------
+  // データによるテスト
+  //---------------------------------------------------------------------------
+  describe.each`
+  data
+  ${D.zero}
+  ${D.one}
+  ${D.s34}
+  ${D.s11}
+  ${D.equi}
+  ${D.acute}
+  ${D.obtuse}
+  `(`Test by data`, ({data}) => {
+
+    describe(`${data.name}:new Triangle(${data.points})`, () => {
+      const t = new Triangle(data.points);
+
+      describe(`constructor(${data.points})`, () => {
+        it(`t.A.x = ${data.points[0]}`, () => { expect(t.A.x).toBe(data.points[0]); })
+        it(`t.A.y = ${data.points[1]}`, () => { expect(t.A.y).toBe(data.points[1]); })
+        it(`t.B.x = ${data.points[2]}`, () => { expect(t.B.x).toBe(data.points[2]); })
+        it(`t.B.y = ${data.points[3]}`, () => { expect(t.B.y).toBe(data.points[3]); })
+        it(`t.C.x = ${data.points[4]}`, () => { expect(t.C.x).toBe(data.points[4]); })
+        it(`t.C.y = ${data.points[5]}`, () => { expect(t.C.y).toBe(data.points[5]); })
+      })
+
+      //-----------------------------------------------------------------------
+      // 3辺と長さ
+      //-----------------------------------------------------------------------
+  
+      describe(`3 sides`, () => {
+        it(`AB = (${data.AB.x}, ${data.AB.y})`, () => {
+          expect(t.AB).toEqual(data.AB);
+        })
+        it(`BC = (${data.BC.x}, ${data.BC.y})`, () => {
+          expect(t.BC).toEqual(data.BC);
+        })
+        it(`CA = (${data.CA.x}, ${data.CA.y})`, () => {
+          expect(t.CA).toEqual(data.CA);
+        })
+      })
+
+      describe(`3 length`, () => {
+        it(`a = ${data.a}`, () => { expect(t.a).toBeCloseTo(data.a); })
+        it(`b = ${data.b}`, () => { expect(t.b).toBeCloseTo(data.b); })
+        it(`c = ${data.c}`, () => { expect(t.c).toBeCloseTo(data.c); })
+      })
+
+      describe(`maxSideName`, () => {
+        it(`maxSideName = ${data.maxSideName}`, () => {
+          expect(t.maxSideName).toEqual(data.maxSideName);
+        })
+      })
+
+      describe(`minSideName`, () => {
+        it(`minSideName = ${data.minSideName}`, () => {
+          expect(t.minSideName).toEqual(data.minSideName);
+        })
+      })
+
+      describe(`maxLength`, () => {
+        it(`maxLength = ${data.maxLength}`, () => {
+          expect(t.maxLength).toBeCloseTo(data.maxLength);
+        })
+      })
+
+      describe(`minLength`, () => {
+        it(`minLength = ${data.minLength}`, () => {
+          expect(t.minLength).toBeCloseTo(data.minLength);
+        })
+      })
+
+      //-----------------------------------------------------------------------
+      // 角の扱い
+      //-----------------------------------------------------------------------
+      describe(`maxCornerName`, () => {
+        it(`maxCornerName = ${data.maxCornerName}`, () => {
+          expect(t.maxCornerName).toEqual(data.maxCornerName);
+        })
+      })
+
+      describe(`minCornerName`, () => {
+        it(`minCornerName = ${data.minCornerName}`, () => {
+          expect(t.minCornerName).toEqual(data.minCornerName);
+        })
+      })
+
+      //-----------------------------------------------------------------------
+      // 三角比
+      //-----------------------------------------------------------------------
+      describe(`cosA`, () => {
+        it(`cosA = ${data.cosA}`, () => {
+          expect(t.cosA).toBeCloseTo(data.cosA);
+        })
+      })
+
+      describe(`cosB`, () => {
+        it(`cosB = ${data.cosB}`, () => {
+          expect(t.cosB).toBeCloseTo(data.cosB);
+        })
+      })
+
+      describe(`cosC`, () => {
+        it(`cosC = ${data.cosC}`, () => {
+          expect(t.cosC).toBeCloseTo(data.cosC);
+        })
+      })
+
+      describe(`sinA`, () => {
+        it(`sinA = ${data.sinA}`, () => {
+          expect(t.sinA).toBeCloseTo(data.sinA);
+        })
+      })
+
+      describe(`sinB`, () => {
+        it(`sinB = ${data.sinB}`, () => {
+          expect(t.sinB).toBeCloseTo(data.sinB);
+        })
+      })
+
+      describe(`sinC`, () => {
+        it(`sinC = ${data.sinC}`, () => {
+          expect(t.sinC).toBeCloseTo(data.sinC);
+        })
+      })
+
+      describe(`tanA`, () => {
+        it(`tanA = ${data.tanA}`, () => {
+          expect(t.tanA).toBeCloseTo(data.tanA);
+        })
+      })
+
+      describe(`tanB`, () => {
+        it(`tanB = ${data.tanB}`, () => {
+          expect(t.tanB).toBeCloseTo(data.tanB);
+        })
+      })
+
+      describe(`tanC`, () => {
+        it(`tanC = ${data.tanC}`, () => {
+          expect(t.tanC).toBeCloseTo(data.tanC);
+        })
+      })
+
+      describe(`maxCornerCos`, () => {
+        it(`maxCornerCos = ${data.maxCornerCos}`, () => {
+          expect(t.maxCornerCos).toBeCloseTo(data.maxCornerCos);
+        })
+      })
+
+      describe(`minCornerCos`, () => {
+        it(`minCornerCos = ${data.minCornerCos}`, () => {
+          expect(t.minCornerCos).toBeCloseTo(data.minCornerCos);
+        })
+      })
+
+      describe(`maxCornerSin`, () => {
+        it(`maxCornerSin = ${data.maxCornerSin}`, () => {
+          expect(t.maxCornerSin).toBeCloseTo(data.maxCornerSin);
+        })
+      })
+
+      describe(`minCornerSin`, () => {
+        it(`minCornerSin = ${data.minCornerSin}`, () => {
+          expect(t.minCornerSin).toBeCloseTo(data.minCornerSin);
+        })
+      })
+
+      describe(`maxCornerTan`, () => {
+        it(`maxCornerTan = ${data.maxCornerTan}`, () => {
+          expect(t.maxCornerTan).toBeCloseTo(data.maxCornerTan);
+        })
+      })
+
+      describe(`minCornerTan`, () => {
+        it(`minCornerTan = ${data.minCornerTan}`, () => {
+          expect(t.minCornerTan).toBeCloseTo(data.minCornerTan);
+        })
+      })
+
+      describe(`radA`, () => {
+        it(`radA = ${data.radA}`, () => {
+          expect(t.radA).toBeCloseTo(data.radA);
+        })
+      })
+
+      describe(`radB`, () => {
+        it(`radB = ${data.radB}`, () => {
+          expect(t.radB).toBeCloseTo(data.radB);
+        })
+      })
+
+      describe(`radC`, () => {
+        it(`radC = ${data.radC}`, () => {
+          expect(t.radC).toBeCloseTo(data.radC);
+        })
+      })
+
+      //-----------------------------------------------------------------------
+      // 幾何学
+      //-----------------------------------------------------------------------
+      describe(`area`, () => {
+        it(`area = ${data.area}`, () => {
+          expect(t.area).toBeCloseTo(data.area);
+        })
+      })
+
+      describe(`outerRadius`, () => {
+        it(`outerRadius = ${data.outerRadius}`, () => {
+          expect(t.outerRadius).toBeCloseTo(data.outerRadius);
+        })
+      })
+
+      describe(`innerRadius`, () => {
+        it(`innerRadius = ${data.innerRadius}`, () => {
+          expect(t.innerRadius).toBeCloseTo(data.innerRadius);
+        })
+      })
+
+      describe(`center`, () => {
+        it(`center = ${data.center.toString()}`, () => {
+          expect(t.center).toEqual(data.center);
+        })
+      })
+
+      describe(`outerCenter`, () => {
+        it(`outerCenter = ${data.outerCenter.toString()}`, () => {
+          expect(t.outerCenter).toEqual(data.outerCenter);
+        })
+      })
+
+      describe(`innerCenter`, () => {
+        it(`innerCenter = ${data.innerCenter.toString()}`, () => {
+          expect(t.innerCenter).toEqual(data.innerCenter);
+        })
+      })
+
+      //-----------------------------------------------------------------------
+      // 三角形の成立条件
+      //-----------------------------------------------------------------------
+      describe(`isInvalid`, () => {
+        it(`isInvalid = ${data.isInvalid}`, () => {   
+          expect(t.isInvalid).toEqual(data.isInvalid); 
+        })
+      })
+
+      describe(`type`, () => {
+        it(`type = ${data.type}`, () => {   
+          expect(t.type).toEqual(data.type); 
+        })
+      })
+
+      describe(`toString()`, () => {
+        it(`toString() = ${data.toString}`, () => {   
+          expect(t.toString()).toEqual(data.toString); 
+        })
+      })
+    })
+  })
+
+  //---------------------------------------------------------------------------
+  // 追加テスト
+  //---------------------------------------------------------------------------
+  describe(`cover line 116, 161`, () => {
+    const f = new Triangle([2, 1, 0, 0, 2, 0]);
+
+    it(`f.minSideName=CA`, () => {
+      expect(f.minSideName).toBe("CA");
+    })
+
+    it(`f.minCornerName=B`, () => {
+      expect(f.minCornerName).toBe("B");
+    })
+  })
+})
---
+++ src/Triangle.ts
@@ -0,0 +1,437 @@
+import Vector2 from './Vector2';
+
+export enum Type {
+  None,   // なし
+  Right,  // 直角
+  Acute,  // 鋭角
+  Obtuse, // 鈍角
+}
+
+/******************************************************************************
+ * 三角形
+ *          * A
+ *         * *
+ *        *   *
+ *       *     *
+ *      *********
+ *     B         C
+ * 
+ * 三角形を構成する３点をそれぞれABCとし、それぞれの角度を∠A、∠B、∠Cとする。
+ * 
+ * この角に対する辺の長さを以下のように定義する
+ * BC = a
+ * CA = b
+ * AB = c
+ *****************************************************************************/
+export default class Triangle {
+  
+  private _A:Vector2;
+  private _B:Vector2;
+  private _C:Vector2;
+
+  constructor(p:number[] = []){
+    const ax = p[0]? p[0]:0;
+    const ay = p[1]? p[1]:0;
+    const bx = p[2]? p[2]:0;
+    const by = p[3]? p[3]:0;
+    const cx = p[4]? p[4]:0;
+    const cy = p[5]? p[5]:0;
+
+    this._A = new Vector2(ax, ay);
+    this._B = new Vector2(bx, by);
+    this._C = new Vector2(cx, cy);
+  }
+
+  get A() { return this._A; }
+  get B() { return this._B; }
+  get C() { return this._C; }
+
+  /** 辺ABのベクトル */
+  get AB() {
+    return Vector2.sub(this.B, this.A);
+  }
+
+  /** 辺BCのベクトル */
+  get BC() {
+    return Vector2.sub(this.C, this.B);
+  }
+
+  /** 辺CAのベクトル */
+  get CA() {
+    return Vector2.sub(this.A, this.C);
+  }
+  
+  /** 辺ABの長さ */
+  get a() {
+    return this.BC.magnitude;
+  }
+
+  /** 辺CAの長さ */
+  get b() {
+    return this.CA.magnitude;
+  }
+
+  /** 辺ABの長さ */
+  get c() {
+    return this.AB.magnitude;
+  }
+
+  //---------------------------------------------------------------------------
+  // 辺の長さの扱い
+  /** 辺の長い順にソートした配列 */
+  get sortedLength() {
+    const { a,  b,  c } = this;
+    const list = [a, b, c];
+    list.sort((a, b) => b - a);
+    return list;
+  }
+
+  /** 最大辺の名前 */
+  get maxSideName() {
+    if(this.isInvalid) return "";
+
+    // 辺の長さが長い順にそーと
+    const list = this.sortedLength;
+    
+    const { a, c } = this;
+    switch(list[0]) {
+      case c : return "AB";
+      case a : return "BC";
+      default: return "CA";
+    }
+  }
+
+  /** 最小辺の名前 */
+  get minSideName() {
+    if(this.isInvalid) return "";
+
+    // 辺の長さが長い順にそーと
+    const list = this.sortedLength;
+    
+    const { a, c } = this;
+
+    switch(list[2]) {
+      case c : return "AB";
+      case a : return "BC";
+      default: return "CA";
+    }
+  }
+
+  /** 名前を指定して辺の長さを取得 */
+  getLengthAt(sideName: string) {
+    switch(sideName) {
+      case "BC": return this.a;
+      case "CA": return this.b;
+      case "AB": return this.c;
+    }
+    return 0;
+  }
+
+  /** 最大辺の長さ */
+  get maxLength() {
+    return this.getLengthAt(this.maxSideName);
+  }
+
+  /** 最小辺の長さ */
+  get minLength() {
+    return this.getLengthAt(this.minSideName);
+  }
+
+  //---------------------------------------------------------------------------
+  // 角度の扱い
+  /** 最大角の名前 */
+  get maxCornerName() {
+    const side = this.maxSideName;
+
+    switch(side){
+      case "BC": return "A";
+      case "CA": return "B";
+      case "AB": return "C";
+    }
+
+    return "";
+  }
+
+  /** 最小角の名前 */
+  get minCornerName() {
+    const side = this.minSideName;
+
+    switch(side) {
+      case "BC": return "A";
+      case "CA": return "B";
+      case "AB": return "C";
+    }
+
+    return "";
+  }
+
+  /** 余剰定理によりcosA */
+  get cosA() {
+    if (this.isInvalid) return 0;
+
+    const { a, b, c } = this;
+
+    const n = (b**2) + (c**2) - (a**2);
+    const d = 2 * b * c;
+    const cos = n/d;
+    return cos;
+  }
+
+  /** 余剰定理によりcosB */
+  get cosB() {
+    if (this.isInvalid) return 0;
+
+    const { a, b, c } = this;
+
+    const n = (c**2) + (a**2) - (b**2);
+    const d = 2 * c * a;
+    const cos = n/d;
+    return cos;
+  }
+
+  /** 余剰定理によりcosC */
+  get cosC() {
+    if (this.isInvalid) return 0;
+
+    const { a, b, c } = this;
+
+    const n = (a**2) + (b**2) - (c**2);
+    const d = 2 * a * b;
+    const cos = n/d;
+    return cos;
+  }
+
+  /** sinA */
+  get sinA() {
+    if (this.isInvalid) return 0;
+
+    const { cosA } = this;
+    const sin = Math.sqrt(1 - (cosA**2));
+    return sin;
+  }
+
+  /** sinB */
+  get sinB() {
+    if (this.isInvalid) return 0;
+
+    const { cosB } = this;
+    const sin = Math.sqrt(1 -  (cosB**2));
+    return sin;
+  }
+
+  /** sinC */
+  get sinC() {
+    if (this.isInvalid) return 0;
+
+    const  { cosC } = this;
+    const sin = Math.sqrt(1 - (cosC**2));
+    return sin;
+  }
+
+  /** tanA */
+  get tanA() {
+    if (this.isInvalid) return 0;
+
+    const { sinA, cosA } = this;
+    return sinA / cosA;
+  }
+
+  /** tanB */
+  get tanB() {
+    if (this.isInvalid) return 0;
+
+    const { sinB, cosB } = this;
+    return sinB / cosB;
+  }
+
+  /** tanC */
+  get tanC() {
+    if (this.isInvalid) return 0;
+
+    const { sinC, cosC } = this;
+    return sinC / Number(cosC.toFixed(15)); // 小数点誤差をごまかすためにfixedでまるめ
+  }
+
+  /** 指定した角の余剰を取得する */
+  getCosAt(cornerName:string) {
+    switch(cornerName) {
+      case "A": return this.cosA;
+      case "B": return this.cosB;
+      case "C": return this.cosC;
+    }
+    return 0;
+  }
+
+  /** 最大角の余剰 */
+  get maxCornerCos() {
+    return this.getCosAt(this.maxCornerName);
+  }
+
+  /** 最小角の余剰 */
+  get minCornerCos() {
+    return this.getCosAt(this.minCornerName);
+  }
+
+  /** 指定した角のsinを取得 */
+  getSinAt(cornerName:string) {
+    switch(cornerName) {
+      case "A": return this.sinA;
+      case "B": return this.sinB;
+      case "C": return this.sinC;
+    }
+    return 0;
+  }
+
+  /** 最大角のsin */
+  get maxCornerSin() {
+    return this.getSinAt(this.maxCornerName);
+  }
+
+  /** 最小角のsin */
+  get minCornerSin() {
+    return this.getSinAt(this.minCornerName);
+  }
+
+  /** 指定した角のtanを取得 */
+  getTanAt(connerName:string) {
+    switch(connerName) {
+      case "A": return this.tanA;
+      case "B": return this.tanB;
+      case "C": return this.tanC;
+    }
+    return 0;
+  }
+
+  /** 最大角のtan */
+  get maxCornerTan() {
+    return this.getTanAt(this.maxCornerName);
+  }
+
+  /** 最小角のtan */
+  get minCornerTan() {
+    return this.getTanAt(this.minCornerName);
+  }
+
+  /** ∠A(弧度法) */
+  get radA() {
+    if (this.isInvalid) return 0;
+    const { cosA } = this;
+    return Math.acos(cosA);
+  }
+
+  /** ∠B(弧度法) */
+  get radB() {
+    if (this.isInvalid) return 0;
+    const { cosB } = this;
+    return Math.acos(cosB);
+  }
+
+  /** ∠C(弧度法) */
+  get radC() {
+    if (this.isInvalid) return 0;
+    const { cosC } = this;
+    return Math.acos(cosC);
+  }
+
+  //---------------------------------------------------------------------------
+  // 幾何学
+
+  /** 三角形の面積 */
+  get area() {
+    const { b, c, sinA } = this;
+    return (b * c * sinA) * 0.5;
+  }
+
+  /** 外接円の半径 */
+  get outerRadius() {
+    if (this.isInvalid) return 0;
+    const { a, sinA } = this;
+    return a / (2*sinA);
+  }
+
+  /** 内接円の半径 */
+  get innerRadius() {
+    if (this.isInvalid) return 0;
+    const { a, b, c, area } = this;
+    return (2 * area) / (a + b + c);
+  }
+
+  /** 三角形の中心(重心) */
+  get center() {
+    if (this.isInvalid) return Vector2.zero;
+
+    const { A, B, C } = this;
+    const g = new Vector2().add(A).add(B).add(C).times(1/3);
+    
+    return g;
+  }
+
+  /** 三角形の外接円の中心(外心) */
+  get outerCenter() {
+    if (this.isInvalid) return Vector2.zero;
+
+    const { A, B, C, radA, radB, radC } = this;
+
+    const sin2A = Math.sin(radA*2);
+    const sin2B = Math.sin(radB*2);
+    const sin2C = Math.sin(radC*2);
+
+    const center = A.clone().times(sin2A)
+      .add(B.clone().times(sin2B))
+      .add(C.clone().times(sin2C));
+
+    const d = sin2A + sin2B + sin2C;
+
+    return center.times(1/d);
+  }
+
+  /** 三角形の内接円の中心(内心) */
+  get innerCenter() {
+    if (this.isInvalid) return Vector2.zero;
+
+    const { A, B, C, a, b, c } = this;
+
+    const center = A.clone().times(a)
+      .add(B.clone().times(b))
+      .add(C.clone().times(c));
+
+    const d = 1 / (a + b + c);
+
+    return center.times(d);
+  }
+
+  //---------------------------------------------------------------------------
+  // 状態
+
+  /** 三角形として無効か */
+  get isInvalid() {
+    const list = this.sortedLength;
+
+    // 一番長い辺が残りの２つの辺の長さの合計より小さければ三角形として成り立つ
+    const [a, b, c] = list;
+    return !(a < b + c);
+  }
+
+  /** 三角形のタイプ */
+  get type() {
+    if (this.isInvalid) return Type.None;
+
+    const list = this.sortedLength;
+
+    const max = Number((list[0]**2).toFixed(15));
+    const mid = Number((list[1]**2).toFixed(15));
+    const min = Number((list[2]**2).toFixed(15));
+
+    // 最大辺 < 他の二辺の二乗の和 -> 鋭角
+    if (max < mid + min) return Type.Acute;
+    // 最大辺 > 他の二辺の二乗の和 -> 鈍角
+    if (max > mid + min) return Type.Obtuse;
+    // 最大辺 = 他の二辺の二乗の和 -> 直角
+    return Type.Right;
+  }
+
+  toString() {
+    const { A, B, C } = this;
+    return `A${A}, B${B}, C${C}`
+  }
+}
