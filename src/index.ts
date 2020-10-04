c3c1045dcc35512145e5c910a6989188979ce9fc
kazumichi kuroda <kazumichi96@gmail.com>
2020-10-04 13:44:28 +0900

    Vector3、Matrix3、Matrix4を追加

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@
  *****************************************************************************/
 import * as Util from "./util";
 import Vector2 from "./Vector2";
+import Vector3 from "./Vector3";
+import Matrix3 from "./Matrix3";
+import Matrix4 from "./Matrix4";
 import Quadratic from "./Quadratic";
 import Linear from "./Linear";
 import Triangle2 from "./Triangle2";
@@ -14,6 +17,9 @@ export {
   Linear, 
   Quadratic,   
   Vector2, 
+  Vector3,
+  Matrix3,
+  Matrix4,
   Triangle2,
   Primitive2,
   Collision2,
