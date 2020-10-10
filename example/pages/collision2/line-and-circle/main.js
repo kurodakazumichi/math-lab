a7d75dd74579970efcce19423f39f1d5b2a31259
kazumichi kuroda <kazumichi96@gmail.com>
2020-10-10 09:43:47 +0900

    Updated 直線と円の衝突

--- example/pages/collision2/line-and-circle/main.js
+++ example/pages/collision2/line-and-circle/main.js
@@ -54,6 +54,70 @@
     }
   }
 
+  class Graph2Base extends Somali.Scene {
+
+    constructor() {
+      super();
+      this.line = new Line(new Vector2(0, -2.5), new Vector2(2, 1));
+      this.circle = new Circle(new Vector2(0, 0), 3);
+      this.col = Collision2.LineAndCircle.intercect(this.line, this.circle);
+    }
+
+    createNodes(shapes, groups) {
+      return {
+        grid: groups.grid(),
+        line: shapes.line(),
+        circle: shapes.circle(),
+        C: shapes.point(),
+        cText: shapes.text("C"),
+        H: shapes.point(),
+        hText: shapes.text("H"),
+        r: shapes.line().dash(5),
+        rText: shapes.text("r"),
+        aux: shapes.aux(),
+      }
+    }
+
+    initNodes(nodes) {
+      Sync.lineToLine(this.line, nodes.line);
+      Sync.circleToCircle(this.circle, nodes.circle);
+      Sync.vecToPos(this.circle.p, nodes.C);
+      Sync.vecToPos(this.col.nearest, nodes.H);
+      Sync.vecToPos(this.circle.p, nodes.cText);
+      Sync.vecToPos(this.col.nearest, nodes.hText);
+
+      const rv = Vector2.right.times(this.circle.r).rotate(Util.deg2rad(27));
+      const mp = Vector2.midpoint(Vector2.zero, rv);
+      Sync.vecToLine(rv, nodes.r);
+      Sync.vecToPos(mp, nodes.rText);
+
+      nodes.aux.points([this.circle.p.x, this.circle.p.y, this.col.nearest.x, this.col.nearest.y]);
+    }
+
+    hide(keys) {
+      keys.map((key) => {
+        this.nodes[key].visible(false);
+      })
+    }
+  }
+
+  graphs.Graph2_1 = class extends Graph2Base {
+    get option() {
+      return {id:"graph2_1"}
+    }
+
+    initNodes(nodes) {
+      super.initNodes(nodes);
+      this.hide(["r", "rText"]);
+    }
+  }
+
+  graphs.Graph2_2 = class extends Graph2Base {
+    get option() {
+      return {id:"graph2_2"}
+    }
+  }
+
   Object.values(graphs).map((graph) => {
     new graph().build();
   });
