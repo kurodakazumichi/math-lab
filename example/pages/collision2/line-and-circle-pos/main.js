f4cc08f464c890ba45c5800932b2be7b4d55d3a1
kazumichi kuroda <kazumichi96@gmail.com>
2020-10-10 13:19:16 +0900

    Updated 直線と円の交点

--- example/pages/collision2/line-and-circle-pos/main.js
+++ example/pages/collision2/line-and-circle-pos/main.js
@@ -2,7 +2,7 @@
   const { Vector2, Collision2, Util } = MathLab;
   const { Line, Circle } = MathLab.Primitive2;
   const { sColor } = Somali;
-  const { Sync } = props;
+  const { Sync, NodesUtil } = props;
   const graphs = {};
 
   graphs.Graph1 = class extends Somali.Scene {
@@ -62,6 +62,100 @@
       result.pos.map((pos, index) => {
         stars[index].pos(pos.x, pos.y);
       });
+    }
+  }
+
+  class Graph2Base extends Somali.Scene 
+  {
+
+    constructor() {
+      super();
+      this.line = new Line(new Vector2(-1, -3.5), new Vector2(2, 1));
+      this.circle = new Circle(new Vector2(0, 0), 4);
+      this.col = Collision2.LineAndCircle.intercect(this.line, this.circle);
+    }
+
+    createNodes(shapes, groups) {
+      return {
+        grid: groups.grid(),
+        line: shapes.line(),
+        circle: shapes.circle(),
+
+        C: shapes.point(),
+        CText: shapes.text("C"),
+        H: shapes.point(),
+        HText: shapes.text("H"),
+        CH: shapes.aux(),
+
+        P1: shapes.point(),
+        P1Text: shapes.text("P"),
+        P2: shapes.point(),
+        P2Text: shapes.text("P'"),
+        
+        r1: shapes.line().strokeWidth(1),
+        r1Text: shapes.text("r").offset(0, 0.4),
+        r2: shapes.line().strokeWidth(1),
+        r2Text: shapes.text("r").offset(-0.5, 0.35),
+
+        vn: shapes.arrow().color(sColor.yellow),
+        vnText: shapes.text("n").offset(0, 0.6),
+      }
+    }
+
+    initNodes(nodes) {
+
+      const c = this.circle.p;
+      const { pos, nearest } = this.col;
+      const n = this.line.v.normalize;
+
+      Sync.lineByLine(nodes.line, this.line);
+      Sync.circleToCircle(this.circle, nodes.circle);
+      Sync.posByVec(nodes.C     , c)
+      Sync.posByVec(nodes.CText , c).offset(0.2, 0.5);;
+      Sync.posByVec(nodes.H     , nearest);
+      Sync.posByVec(nodes.HText , nearest);
+      Sync.posByVec(nodes.P1    , pos[0]);
+      Sync.posByVec(nodes.P1Text, pos[0]);
+      Sync.posByVec(nodes.P2    , pos[1]);
+      Sync.posByVec(nodes.P2Text, pos[1]).offset(0, -0.4);
+
+      Sync.posByVec(nodes.r1Text, Vector2.midpoint(c, pos[0]));
+      Sync.posByVec(nodes.r2Text, Vector2.midpoint(c, pos[1]));
+
+      Sync.lineByP1P2(nodes.r1, c, pos[0]);
+      Sync.lineByP1P2(nodes.r2, c, pos[1]);
+      Sync.lineByP1P2(nodes.CH, c, nearest);
+
+      Sync.arrowByPV(nodes.vn, this.col.nearest, n);
+      Sync.posByVec(nodes.vnText, Vector2.add(this.col.nearest, n));
+    }
+  }
+
+  graphs.Graph2 = class extends Graph2Base {
+    get option() {
+      return { id: "graph2_1", update: false}
+    }
+
+    initNodes(nodes) {
+      super.initNodes(nodes);
+      return;
+      NodesUtil.hide(this.nodes, [
+        "C",
+        "CText",
+        "H",
+        "HText",
+        "CH",
+        "P1",
+        "P1Text",
+        "P2",
+        "P2Text",
+        "r1",
+        "r1Text",
+        "r2",
+        "r2Text",
+        "vn",
+        "vnText",
+      ]);
     }
   }
 
